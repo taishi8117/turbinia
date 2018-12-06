744fe1349bae5fc33b655a3daf6ade1d838f1d44
Eric Zinnikas <ericwz@fb.com>
2018-12-05 16:41:17 -0800

    bump version

--- turbinia/__init__.py
+++ turbinia/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 """Main Turbinia application."""
 
-__version__ = '20181004+fb20181123'
+__version__ = '20181004+fb20181205'
 
 
 class TurbiniaException(Exception):
